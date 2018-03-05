fbe355b9b65fce38b8363c56abab62d629f5938c
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-03-05 18:03:53 -0300

    Create EXERCICIOS_DECISAO.py

---
+++ EXERCICIOS_DECISAO.py
@@ -0,0 +1,8 @@
+def ex1():
+    n1 = int(input("digite um numero inteiro: \n"))
+    n2 = int(input("digite outro numero inteiro: \n"))
+
+    if n1 > n2:
+        print("o número {} é maior".format(n1))
+    else:
+        print("o numero {} maior".format(n2))
