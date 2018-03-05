c3303c623f9a9ef636d4424c273afc54f84776b6
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-03-05 18:08:31 -0300

    Update EXERCICIOS_DECISAO.py

--- EXERCICIOS_DECISAO.py
+++ EXERCICIOS_DECISAO.py
@@ -6,3 +6,11 @@ def ex1():
         print("o número {} é maior".format(n1))
     else:
         print("o numero {} maior".format(n2))
+        
+def ex2():
+    valor = int(input("digite um numero, podendo ser positivo ou negativo: \n"))
+    if valor > 0:
+        print("o numero {} é positivo".format(valor))
+    else:
+        print("o numero {} é negativo".format(valor))
+
