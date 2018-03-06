2a1678613b7629599564c04b94374c47f84370d9
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-03-06 17:55:03 -0300

    Update EXERCICIOS_DECISAO.py

--- EXERCICIOS_DECISAO.py
+++ EXERCICIOS_DECISAO.py
@@ -31,3 +31,14 @@ def ex4():
     else:
         print("esta letra é uma consoante")
 
+def ex5():
+    n1 = float(input("digite a primeira nota: \n"))
+    n2 = float(input("digite a segunda nota: \n"))
+    media = (n1 + n2)/2
+
+    if media >=7:
+        print("Aluno aprovado")
+        if media == 10:
+            print("Aprovado com distinção")
+    else:
+        print("Aluno reprovado")
