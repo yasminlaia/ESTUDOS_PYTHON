4c1bece8460228553d9f6ff5f5939f30ae777380
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-03-28 19:12:54 -0300

    Update EXERCICIOS_SEQUENCIAL.py

--- EXERCICIOS_SEQUENCIAL.py
+++ EXERCICIOS_SEQUENCIAL.py
@@ -114,7 +114,14 @@ def ex15():
     print("o total de desconto de sindicato é:{}\n".format(sindicato))
     print("o salario liquido é:{}\n".format(salario_liquido))
 
+def ex13():
+    semana = int(input("digite um numero de 1 a 7 para escolher o dia da semana:\n"))
+    semana = {'1':'Domingo', '2':'Segunda', '3':'Terça', '4':'Quarta, '5': Quinta, '6':Sexta, '7':Sabado}
 
+    if semana == 1:
+       print(semana['1'])
+    else:
+       print('valor invalido')
     
     
     
